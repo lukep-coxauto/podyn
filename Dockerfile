145e51c3ebc978264a0eaec31259cf8ac4ce592f
Justin Zaun <jzaun@viserion.local>
2020-09-03 09:14:55 -1000

    Trying to auto restart the podyn service.

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,18 @@ FROM openjdk:8
 ARG APP_VERSION=1.0
 WORKDIR /app
 COPY --from=builder "/app/target/podyn-${APP_VERSION}.jar" /app/podyn.jar
+COPY stop.sh /app/
+RUN chmod +x /app/stop.sh
 COPY entrypoint.sh /
-RUN chmod +x /entrypoint.sh  
+RUN chmod +x /entrypoint.sh
+COPY podyn-service /etc/init.d/podyn
+RUN chmod +x /etc/init.d/podyn
+
+RUN apt-get update
+RUN apt-get install cron -y
+COPY podyn-cron /etc/cron.d/podyn-cron
+RUN chmod 0644 /etc/cron.d/podyn-cron
+RUN crontab /etc/cron.d/podyn-cron
+RUN touch /var/log/cron.log
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,29 @@ else
     DYNAMODB_OPTS=""
 fi
 
-exec java -jar /app/podyn.jar --postgres-jdbc-url "${POSTGRES_JDBC_URL}" $DYNAMODB_OPTS "$@"
+# Create a launcher script to stop a running podyn and start a new one
+
+echo "#!/bin/bash" > ./launch.sh
+echo "/app/stop.sh podyn" >> ./launch.sh
+echo "if [ \$AWS_SECRET_ACCESS_KEY != \"test\" ]; then" >> ./launch.sh
+echo "  curl -X POST --data-urlencode 'payload={\"username\":\"podyn\",\"channel\":\"developers\",\"icon_url\":\"\",\"attachments\":[{\"color\":\"#4183c4\",\"text\":\"Launching podyn ${AWS_REGION}\",\"footer\":\"\"}]}' https://hooks.slack.com/services/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" >> ./launch.sh
+echo "fi" >> ./launch.sh
+echo "exec java -jar /app/podyn.jar --postgres-jdbc-url \"${POSTGRES_JDBC_URL}\" $DYNAMODB_OPTS ${@}" >> ./launch.sh
+
+chmod +x ./launch.sh
+
+echo "---- env"
+env
+echo "----"
+
+
+if [[ ${@} != *"--data"* ]]; then
+    # Start the syncer
+    touch /app/podyn.log
+    service podyn start
+    tail -f /app/podyn.log
+else
+    # Just load the data and exit
+    /app/launch.sh
+fi
+
