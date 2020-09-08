133f799f5f727a0d351110010369506a8ff0911b
Justin Zaun <jzaun@viserion.local>
2020-09-08 11:27:22 -1000

    Get the Slack webhook from the entrypoint command line

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,15 @@ else
     DYNAMODB_OPTS=""
 fi
 
+PODYN=$@
+if [[ "$@" != "--"* ]]; then
+    SLACK=`echo $@ | sed 's/ .*//'`
+    PODYN=`echo $@ | sed 's/.* //'`
+fi
+
+echo "Slack: $SLACK"
+echo "Podyn: $PODYN"
+
 # Create a launcher script to stop a running podyn and start a new one
 
 echo "#!/bin/bash" > ./launch.sh
@@ -16,8 +25,8 @@ echo "else" >> ./launch.sh
 echo "    export AWS_SECRET_ACCESS_KEY=\"$AWS_SECRET_ACCESS_KEY\"" >> ./launch.sh
 echo "    export AWS_ACCESS_KEY_ID=\"$AWS_ACCESS_KEY_ID\"" >> ./launch.sh
 echo "    export AWS_REGION=\"$AWS_REGION\"" >> ./launch.sh
-echo "    if [ \"\$AWS_SECRET_ACCESS_KEY\" == \"test\" ]; then" >> ./launch.sh
-echo "        curl -X POST --data-urlencode 'payload={\"username\":\"podyn\",\"channel\":\"developers\",\"icon_url\":\"\",\"attachments\":[{\"color\":\"#4183c4\",\"text\":\"Launching podyn ${AWS_REGION}\",\"footer\":\"\"}]}' https://hooks.slack.com/services/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" >> ./launch.sh
+echo "    if [ \"$SLACK\" != \"\" ]; then" >> ./launch.sh
+echo "        curl -X POST --data-urlencode 'payload={\"username\":\"podyn\",\"channel\":\"developers\",\"icon_url\":\"\",\"attachments\":[{\"color\":\"#4183c4\",\"text\":\"Launching podyn ${AWS_REGION}\",\"footer\":\"\"}]}' $SLACK" >> ./launch.sh
 echo "    else" >> ./launch.sh
 echo "        echo Restarting..." >> ./launch.sh
 echo "    fi" >> ./launch.sh
